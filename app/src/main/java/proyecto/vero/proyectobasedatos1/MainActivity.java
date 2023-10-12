1dc401e4fde3bee08f72b1697ddab5f031895cab
unknown <sistemas@CHI051PCAL008.alumnos.cst>
2023-10-12 16:41:50 -0300

    firebase

---
+++ app/src/main/java/proyecto/vero/proyectobasedatos1/MainActivity.java
@@ -0,0 +1,63 @@
+package proyecto.vero.proyectobasedatos1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button botonEnviar;
+    EditText mensaje;
+
+    TextView recibir;
+
+    FirebaseDatabase firebaseDatabase;
+
+    DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botonEnviar = findViewById(R.id.btnenviar);
+        mensaje = findViewById(R.id.txtingresar);
+        recibir = findViewById(R.id.txtrecibir);
+
+        firebaseDatabase = firebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+
+        botonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                databaseReference.child("Mensaje").setValue(mensaje.getText().toString());
+            }
+        });
+
+        databaseReference.child("Mensaje").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String datos = snapshot.getValue(String.class);
+                recibir.setText(datos);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+}
